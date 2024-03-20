faeff86c7d3fa6d53f3266b83e473bed0dc3e4a5
mayank-sheoran <m1y1nksheor1n@gmail.com>
2024-03-20 12:46:00 +0530

    UI fixer

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,64 +1,78 @@
-import ChartScreen from "../../components/charting";
+import React, { useState } from 'react';
 import Screener from "../../components/screeners/screener";
-import {useState} from "react";
-
 
 export default function Dashboard() {
-    const [chartData, setChartData] = useState([])
+    const [chartData, setChartData] = useState([]);
+
     return (
-        <div style={{
-            display: 'flex',
-            flexDirection: 'row',
-            justifyContent: 'center',
-            alignItems: 'center',
-            height: '100vh',
-            width: '100vw',
-        }}>
-            {/*<div style={{*/}
-            {/*    display: 'flex',*/}
-            {/*    flex: 1,*/}
-            {/*    height: '100vh',*/}
-            {/*    width: '100vw',*/}
-            {/*    justifyContent: 'center',*/}
-            {/*    alignItems: 'center',*/}
-            {/*}} >*/}
-            {/*    <ChartScreen chartData={chartData}/>*/}
-            {/*</div>*/}
+        <>
             <div style={{
                 display: 'flex',
-                flexDirection: 'column',
-                padding: "5px",
-                width: '80vw',
-                border: "solid 1px #ccc",
-                borderTopLeftRadius: '5px',
-                borderBottomLeftRadius: '5px',
-                height: '100vh'
+                flexDirection: 'row',
+                justifyContent: 'center',
+                alignItems: 'center',
+                height: '100vh',
+                width: '100vw',
             }}>
                 <div style={{
-                    display: "flex",
+                    display: 'flex',
                     flexDirection: 'column',
-                    flex: 1,
-                    fontSize: '10px',
-                    justifyContent: 'start',
-                    alignItems: 'center',
-                    borderBottom: "solid 0.5px #ccc"
+                    padding: "5px",
+                    width: '95vw',
+                    border: "solid 1px #ccc",
+                    borderTopLeftRadius: '5px',
+                    borderBottomLeftRadius: '5px',
+                    height: '100vh',
                 }}>
-                    <p style={{padding: 2, margin: 2, alignSelf: 'start', fontWeight: 'bold', color: '#2268ff', fontSize: '16px'}}>Day range breakout</p>
-                    <Screener setChartData={setChartData} webSocketUrl={"/day-range-breakout"}/>
-                </div>
-                <div style={{
-                    display: "flex",
-                    flexDirection: 'column',
-                    flex: 1,
-                    fontSize: '10px',
-                    justifyContent: 'start',
-                    alignItems: 'center',
-                    borderTop: "solid 0.5px #ccc"
-                }}>
-                    <p style={{padding: 2, margin: 2, alignSelf: 'start', fontWeight: 'bold', color: '#2268ff', fontSize: '16px'}}>Candle reversal pattern</p>
-                    <Screener setChartData={setChartData} webSocketUrl={"/pattern/candle-reversal"}/>
+                    <style>
+                        {`
+                    .custom-scrollbar::-webkit-scrollbar {
+                        width: 5px !important;
+                    }
+                    .custom-scrollbar::-webkit-scrollbar-thumb {
+                        background-color: darkslategrey !important;
+                        border-radius: 10px !important;
+                    }
+                    .custom-scrollbar::-webkit-scrollbar-track {
+                        background: #ccc !important;
+                    }
+                `}
+                    </style>
+                    <div style={{
+                        display: "flex",
+                        flexDirection: 'column',
+                        flex: 1,
+                        fontSize: '10px',
+                        justifyContent: 'start',
+                        overflow: 'auto',
+                        alignItems: 'center',
+                        borderBottom: "solid 0.5px #ccc",
+                    }} className="custom-scrollbar">
+                        <p style={{padding: 2, margin: 2, fontWeight: 'bold', color: '#2268ff', fontSize: '16px'}}>Day
+                            range breakout</p>
+                        <Screener setChartData={setChartData} webSocketUrl={"/day-range-breakout"}/>
+                    </div>
+                    <div style={{
+                        display: "flex",
+                        flexDirection: 'column',
+                        flex: 1,
+                        fontSize: '10px',
+                        overflow: 'auto',
+                        justifyContent: 'start',
+                        alignItems: 'center',
+                        borderTop: "solid 0.5px #ccc",
+                    }} className="custom-scrollbar">
+                        <p style={{
+                            padding: 2,
+                            margin: 2,
+                            fontWeight: 'bold',
+                            color: '#2268ff',
+                            fontSize: '16px'
+                        }}>Candle reversal pattern</p>
+                        <Screener setChartData={setChartData} webSocketUrl={"/pattern/candle-reversal"}/>
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
     );
 }
