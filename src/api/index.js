11c8592acb9fbaffabb5f6c6daa0dde82011ccf6
mayank-sheoran <m1y1nksheor1n@gmail.com>
2024-04-22 09:23:33 +0530

    chhanged base url

--- src/api/index.js
+++ src/api/index.js
@@ -2,9 +2,10 @@ import axios from "axios";
 
 // export const BASE_URL = 'http://localhost:8090';
 // export const WS_BASE_URL = 'localhost:8090';
-export const BASE_URL = 'https://api.algotuners.com';
-export const WS_BASE_URL = 'api.algotuners.com/ws/screeners';
-// export const BASE_URL = 'https://portfolio-manager-production.up.railway.app';
+// export const BASE_URL = 'https://api.algotuners.com';
+// export const WS_BASE_URL = 'api.algotuners.com/ws/screeners';
+export const WS_BASE_URL = 'gomarketwatch-production.up.railway.app/ws/screeners';
+export const BASE_URL = 'https://gomarketwatch-production.up.railway.app/';
 export const api = axios.create({
     baseURL: BASE_URL,
     headers: {
