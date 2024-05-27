0555a3649d51c13db124fd760c3ed1835db9bb81
mayank-sheoran <m1y1nksheor1n@gmail.com>
2024-05-27 09:23:48 +0530

    render server

--- src/api/index.js
+++ src/api/index.js
@@ -4,12 +4,12 @@ import axios from "axios";
 // export const WS_BASE_URL = 'localhost:8090';
 // export const BASE_URL = 'https://api.algotuners.com';
 // export const WS_BASE_URL = 'api.algotuners.com/ws/screeners';
-export const BASE_URL = 'https://api-aws.algotuners.com';
-export const WS_BASE_URL = 'api-aws.algotuners.com/ws/screeners';
+// export const BASE_URL = 'https://api-aws.algotuners.com';
+// export const WS_BASE_URL = 'api-aws.algotuners.com/ws/screeners';
 // export const WS_BASE_URL = 'gomarketwatch-production.up.railway.app/ws/screeners';
 // export const BASE_URL = 'https://gomarketwatch-production.up.railway.app/';
-// export const WS_BASE_URL = 'market-watch-1-0.onrender.com/ws/screeners';
-// export const BASE_URL = 'https://market-watch-1-0.onrender.com/';
+export const WS_BASE_URL = 'go-market-watch.onrender.com/ws/screeners';
+export const BASE_URL = 'https://go-market-watch.onrender.com';
 
 export const api = axios.create({
     baseURL: BASE_URL,
