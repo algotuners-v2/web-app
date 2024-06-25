ce2b37379b5e2b4c0aac5e0dea07c57478ef1e75
mayank-sheoran <m1y1nksheor1n@gmail.com>
2024-06-25 09:30:49 +0530

    change server

--- src/api/index.js
+++ src/api/index.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
-export const BASE_URL = 'http://localhost:8090';
-export const WS_BASE_URL = 'localhost:8090/ws/screeners';
+// export const BASE_URL = 'http://localhost:8090';
+// export const WS_BASE_URL = 'localhost:8090/ws/screeners';
 // export const BASE_URL = 'https://api.algotuners.com';
 // export const WS_BASE_URL = 'api.algotuners.com/ws/screeners';
-// export const BASE_URL = 'https://api-gcp.algotuners.com';
-// export const WS_BASE_URL = 'api-gcp.algotuners.com/ws/screeners';
+export const BASE_URL = 'https://api-gcp.algotuners.com';
+export const WS_BASE_URL = 'api-gcp.algotuners.com/ws/screeners';
 // export const WS_BASE_URL = 'gomarketwatch-production.up.railway.app/ws/screeners';
 // export const BASE_URL = 'https://gomarketwatch-production.up.railway.app/';
 // export const WS_BASE_URL = 'market-watch.onrender.com/ws/screeners';
