c14d689eda7f164bd8fba3f7a7076088246e0990
mayank-sheoran <m1y1nksheor1n@gmail.com>
2024-06-25 21:48:15 +0530

    change server

--- src/api/index.js
+++ src/api/index.js
@@ -10,8 +10,8 @@ import axios from "axios";
 // export const BASE_URL = 'https://gomarketwatch-production.up.railway.app/';
 // export const WS_BASE_URL = 'market-watch.onrender.com/ws/screeners';
 // export const BASE_URL = 'https://market-watch.onrender.com';
-export const WS_BASE_URL = '10.190.0.5:8090/ws/screeners';
-export const BASE_URL = '10.190.0.5:8090';
+export const WS_BASE_URL = 'http://10.190.0.5:8090/ws/screeners';
+export const BASE_URL = 'http://10.190.0.5:8090';
 
 export const api = axios.create({
     baseURL: BASE_URL,
